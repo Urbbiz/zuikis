b4000e6aa554f6b05ec5bfdc621c6d298ce01d14
A-Kija <arvydas.kijakauskas@gmail.com>
2021-02-08 12:34:32 +0200

    pirmas

---
+++ index.php
@@ -0,0 +1,19 @@
+<h1>Labas, Zuiki, ka tu?</h1>
+<?php  
+// $kintamasis = 12;
+// $pirmas = 'bla bla';
+// $antras = 'ku kū';
+// $trecias = $pirmas . $antras;
+?>
+<h1>Normaliai</h1>
+<?php  
+// echo $trecias;
+
+
+$pirmas = 'bla bla';
+$antras = "ku  $pirmas  kū";
+
+echo $pirmas;
+echo '<br>';
+echo $antras;
+
