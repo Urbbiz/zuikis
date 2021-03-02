13220bb81cd0d03156534ac75491a6d7c03b4e76
A-Kija <arvydas.kijakauskas@gmail.com>
2021-03-02 13:33:44 +0200

    viskas

--- login/bootstrap.php
+++ login/bootstrap.php
@@ -4,4 +4,7 @@ session_start();
 define('URL', 'http://localhost/zuikis/login/'); // <--- konstanta
 
 
+_d($_SESSION, 'SESIJA--->');
+
+
 
--- login/login.php
+++ login/login.php
@@ -1,8 +1,27 @@
 <?php 
 require __DIR__.'/bootstrap.php';
 
+// LOGOUT scenarijus
+if (isset($_GET['logout'])) {
+    //keli budai
+    // $_SESSION['login'] = 0;
+    // unset($_SESSION['user']);
 
-// POST metodo scenarijus
+    // kitas
+    session_destroy();
+    header('Location: '.URL.'login.php');
+    die;
+}
+
+
+// Jau prisijungusio vartotojo scenarijus
+if(isset($_SESSION['login']) && 1 == $_SESSION['login']) {
+    header('Location: '.URL.'private.php');
+    die;
+}
+
+
+// POST metodo scenarijus LOGIN
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $users = file_get_contents(__DIR__.'/users.json');
@@ -14,6 +33,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     foreach ($users as $user) {
         if ($postName == $user['name']) { // <--- turim useri
             if (password_verify($postPass, $user['pass'])) { // <--- viskas OK
+                 // sugalvojam kad 1 reiks prisijungusi vartotoja
+                //  o 0 reiks neprisijungusi
                 $_SESSION['login'] = 1;
                 $_SESSION['user'] = $user;
                 header('Location: '.URL.'private.php');
@@ -21,16 +42,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
         }
     }
-
-    $_SESSION['msg'] = 'Password or Name is invalid.';
+    $_SESSION['error_msg'] = 'Password or Name is invalid.';
     header('Location: '.URL.'login.php');
     die;
-
-
 }
 
 
 
+// Prisijungimo formos rodymo scenarijus
 
 ?>
 <!DOCTYPE html>
@@ -43,6 +62,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </head>
 <body>
     <h1>Login page</h1>
+    <!-- tikrinam ar yra pranesimas -->
+    <?php if(isset($_SESSION['error_msg'])): ?> 
+        <!-- pranesimas yra. atvaizduojame -->
+        <h3 style="color:red"><?= $_SESSION['error_msg'] ?></h3>
+        <!-- atvaizdavome. nebereikalingas istrinam, kad nerodytu sekati karta -->
+        <?php unset($_SESSION['error_msg']) ?>
+    <?php endif ?>
     <form action="<?= URL ?>login.php" method="post">
         NAME:<input type="text" name="name">
         PASSWORD:<input type="password" name="pass">
