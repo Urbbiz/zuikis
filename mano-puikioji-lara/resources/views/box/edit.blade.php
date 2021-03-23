186c4bfec82926723d90230079860d1b5873d166
A-Kija <arvydas.kijakauskas@gmail.com>
2021-03-23 12:42:03 +0200

    delete

--- mano-puikioji-lara/resources/views/box/edit.blade.php
+++ mano-puikioji-lara/resources/views/box/edit.blade.php
@@ -1,5 +1,5 @@
 <form action="{{route('box.update', $box)}}" method="post">
-
+yra tiek {{$box->bananas}} bananu
 <input type="text" name="bananas_in_box" value="{{$box->bananas}}">
 
 <button type="submit">Edit Box</button>
--- mano-puikioji-lara/resources/views/box/index.blade.php
+++ mano-puikioji-lara/resources/views/box/index.blade.php
@@ -4,6 +4,10 @@
         ID: {{$box->id}} Bananaas in box: {{$box->bananas}}
         <a href="{{route('box.edit', $box)}}">[EDIT]</a>
         <a href="{{route('box.add', $box)}}">[ADD]</a>
+        <form action={{route('box.destroy', $box)}} method="post">
+        <button type="submit">DELETE</button>
+        @csrf
+        </form>
     </li>
 @endforeach
 </ul>
